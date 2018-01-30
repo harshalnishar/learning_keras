28e76a716aec373d51b6aeabae7d19489c164047
Harshal Nishar <harshalnishar@gmail.com>
2018-01-31 00:20:54 +0530

    Added option to save the trained model while running for first time

--- model_train.py
+++ model_train.py
@@ -1,28 +1,42 @@
 import numpy as np
-from keras.models import Sequential
+from keras.models import Sequential, model_from_json
 from keras.layers import Dense
+import os.path
 
 dataset = np.loadtxt("pima-indians-diabetes.csv", delimiter = ',')
 X = dataset[:, 0:8]
 Y = dataset[:,8]
-print(X.shape)
 
-# Model defition:
-# Input layer  : 8 nodes
-# Hidden layer1: 15 nodes
-# Hidden layer2: 10 nodes
-# Output layer : 1 node (Binary classification)
+if not os.path.isfile('dnn_model.json'):
+    # Model defition:
+    # Input layer  : 8 nodes
+    # Hidden layer1: 15 nodes
+    # Hidden layer2: 10 nodes
+    # Output layer : 1 node (Binary classification)
+    dnn_model = Sequential()
+    dnn_model.add(Dense(15, input_dim = 8, activation = 'relu'))
+    dnn_model.add(Dense(10, activation = 'relu'))
+    dnn_model.add(Dense(1, activation = 'sigmoid'))
 
-dnn_model = Sequential()
-dnn_model.add(Dense(15, input_dim = 8, activation = 'relu'))
-dnn_model.add(Dense(10, activation = 'relu'))
-dnn_model.add(Dense(1, activation = 'sigmoid'))
+    dnn_model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 
-dnn_model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+    # Training
+    dnn_model.fit(X, Y, epochs = 1000, batch_size = 10)
+    print(dnn_model.evaluate(X, Y))
 
-dnn_model.fit(X, Y, epochs = 300, batch_size = 10)
+    # Save model
+    json_string = dnn_model.to_json()
+    json_file = open('dnn_model.json', 'w')
+    json_file.write(json_string)
+    json_file.close()
 
-print(dnn_model.evaluate(X, Y))
+    dnn_model.save_weights('dnn_weights.hdf5')
+
+json_file = open('dnn_model.json', 'r')
+json_string = json_file.read()
+json_file.close()
+dnn_model = model_from_json(json_string)
+dnn_model.load_weights('dnn_weights.hdf5')
 
 predict = dnn_model.predict(np.reshape(X[1, :],(1,8)))
 
